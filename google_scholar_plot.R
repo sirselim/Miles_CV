7dff9eef72e6ae5d96ce06aa9dc5c7ef8e78a14f
sirselim <miles.benton84@gmail.com>
2017-07-12 15:29:10 +1200

    updated CV

---
+++ google_scholar_plot.R
@@ -0,0 +1,17 @@
+#!/home/miles/bin/R-devel/bin/Rscript
+
+# install.packages('scholar')
+library(scholar)
+library(ggplot2)
+
+cit <- get_citation_history('iEhBavEAAAAJ')
+
+png('/home/miles/gitrepos/Miles_CV/scholar_plot/scholar_citations_iEhBavEAAAAJ.png', width=800, height=300, res=150)
+ggplot(cit,aes(x=year,y=cites))+
+  geom_bar(stat='identity')+
+  theme_bw()+
+  xlab('Year of citation')+
+  ylab('Google Scholar\n cites')+
+  annotate('text',label=format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z"),x=-Inf,y=Inf,vjust=1.5,hjust=-0.05,size=3,colour='gray')
+dev.off()
+## END
---
+++ process.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "...Pulling Google Scholar data and creating citation plot..."
+# create citation plot
+/home/miles/svn/R/r-devel/build/bin/Rscript google_scholar_plot.R
+# sync plot up to Google Drive
+gdrive sync upload scholar_plot/ 0B-V32YNJHBSgbjdFTndOMlBub2M
+#
+echo "...Done..."
